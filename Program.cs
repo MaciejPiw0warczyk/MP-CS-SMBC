c69bf682d47c937b2fc484573a345a97e4ed0ac4
Maciej <mpiwo4@gmail.com>
2022-05-24 18:12:33 +0200

    added point, player and Time display system

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace SMBClone
         public Point pos, posPast, collOld, collNew, pointsText;
         public int airTime, floor, points, time;
         public float veloHor, veloVert;
-        public bool pressed, midAir;
+        public bool pressed, midAir, natan;
 
         static int Main(string[] args)
         {
@@ -26,6 +26,8 @@ namespace SMBClone
 
         public override void OnCreate()
         {
+            Enable(Subsystem.HrText);
+
             reset();
         }
         public override void OnUpdate(float elapsed)
@@ -88,7 +90,18 @@ namespace SMBClone
 
         private void drawAgent()
         {
-            DrawText(new Point(10, 10), "Score: "+points, Pixel.Presets.White);
+            //draw Player
+            DrawTextHr(new Point(200, 10), player(), Pixel.Presets.White,2);
+            //draw Points
+            DrawTextHr(new Point(450, 10), "Score: " + points, Pixel.Presets.Blue, 2);
+            //draw Time todo
+            DrawTextHr(new Point(700, 10), "Time: " + points, Pixel.Presets.Beige, 2);
+
+
+            DrawLine(new Point(50,0), new Point(50, ScreenHeight),Pixel.Presets.Black);
+            DrawLine(new Point(100, 0), new Point(100, ScreenHeight), Pixel.Presets.Black);
+            DrawLine(new Point(150, 0), new Point(150, ScreenHeight), Pixel.Presets.Black);
+
 
             DrawLine(new Point(0, floor), new Point (ScreenHeight, floor), Pixel.Presets.Brown);
 
@@ -104,6 +117,7 @@ namespace SMBClone
             time = 0;
             pointsText=new Point(10, 10);
             pos = new Point(ScreenWidth/2, floor);
+            natan = true;
         }
 
         private void debugLog()
@@ -136,6 +150,7 @@ namespace SMBClone
                 case Key.Up:
                     if (veloVert > -10)
                         veloVert -= 1;
+                    midAir = true;
                     pressed = true;
                     break;
 
@@ -152,9 +167,21 @@ namespace SMBClone
                 case Key.K8:
                     points++;
                     break;
-            }
+
+                case Key.P:
+                    natan=!natan;
+                    break;
+            }   
+
         }
 
+        private string player()
+        {
+            if (natan)
+                return "Natan";
+            else
+                return "Tomek";
+        }
         private bool colision(Point startingPoint, float velocity, int collisionPoint)
         {
             int endPoint=startingPoint.Y+(int)velocity;
